0ed56cba9d9bb4ecbfd33b7baf709dbc8bc4539a
Olivier Bruch <olivier.bruch@gmail.com>
2022-02-03 10:47:38 +0000

    added links to the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,31 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import { useContext } from 'react'
-import { SidebarOpenContext } from '../lib/SidebarProvider'
+import { SidebarOpenContext, SidebarUpdateContext } from '../lib/SidebarProvider'
 import styles from '../styles/Sidebar.module.css'
 
 const Sidebar = () => {
   const isOpen = useContext(SidebarOpenContext)
+  const close = useContext(SidebarUpdateContext)
 
   return (
     <div hidden={!isOpen} className={styles.sidebar}>
-      sidebar
+      <div>
+        <button className="icon-btn" onClick={close}>
+          <Image src="/icons/menu.svg" width={30} height={30}/>
+        </button>
+        <h2>Menu</h2>
+      </div>
+
+      <div>
+        <Link href="/about">About</Link>
+      </div>
+      <div>
+        <Link href="/login">Login</Link>
+      </div>
+      <div>
+        <Link href="/signup">Signup</Link>
+      </div>
     </div>
   )
 }
