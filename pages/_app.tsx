251b4d538dc3720ce0106347789fef3f893c7019
Olivier Bruch <olivier.bruch@gmail.com>
2022-02-03 11:00:54 +0000

    added header and sidebar to all routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+import SidebarProvider from '../lib/SidebarProvider'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SidebarProvider>
+      <Header></Header>
+      <Sidebar></Sidebar>
+      
+      <div className='page'>
+        <div className="page-content">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </SidebarProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
 import type { NextPage } from 'next'
-import Header from '../components/Header'
-import Sidebar from '../components/Sidebar'
-import SidebarProvider from '../lib/SidebarProvider'
 
 const Home: NextPage = () => {
   return (
-    <SidebarProvider>
-      <Header></Header>
-      <Sidebar></Sidebar>
-    </SidebarProvider>
+    <p>hello world</p>
   )
 }
 
