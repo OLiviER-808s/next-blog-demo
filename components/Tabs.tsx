3f181b07062fda24836660c143eb1a5e304a93f3
Olivier Bruch <olivier.bruch@gmail.com>
2022-02-06 12:27:24 +0000

    fixed tabs

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -4,15 +4,9 @@ export const TabGroup = (props: any) => {
   return (
     <div className={styles.tabs}>
       {props.children.map((tab: any, idx: number) => {
-        return (<>
-          <div hidden key={idx}></div>
-          <input type="radio" className={styles.tab_radio} name={props.name} id={tab.props.id} defaultChecked={idx === 0} />
-          <label htmlFor={tab.props.id} className={styles.tab_label}>{ tab.props.label }</label>
-
-          <div className={styles.tab_content}>
-            { tab.props.children }
-          </div>
-        </>)
+        return (
+          <Tab key={idx} name={props.name} id={tab.props.id} idx={idx} label={tab.props.label} children={tab.props.children}/>
+        )
       })}
     </div>
   )
@@ -20,6 +14,13 @@ export const TabGroup = (props: any) => {
 
 export const Tab = (props: any) => {
   return (
-    <></>
+    <>
+      <input type="radio" className={styles.tab_radio} name={props.name} id={props.id} defaultChecked={props.idx === 0} />
+      <label htmlFor={props.id} className={styles.tab_label}>{ props.label }</label>
+
+      <div className={styles.tab_content}>
+        { props.children }
+      </div>
+    </>
   )
 }
