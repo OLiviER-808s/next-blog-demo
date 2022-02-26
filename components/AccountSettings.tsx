84fabde1e84e1fb01d5d94802c79b02c9165e32c
Olivier Bruch <olivier.bruch@gmail.com>
2022-02-26 14:41:38 +0000

    moved account settings to its own component

---
+++ components/AccountSettings.tsx
@@ -0,0 +1,53 @@
+import Button from "./Button";
+import MoonIcon from '../public/icons/moon18.svg'
+import SunIcon from '../public/icons/sun18.svg'
+import ProfileIcon from '../public/icons/profile.svg'
+import LogoutIcon from '../public/icons/logout18.svg'
+import DeleteIcon from '../public/icons/delete18.svg'
+import HoldButton from "./HoldButton";
+import DeleteBar from "./DeleteBar";
+import { ThemeUpdateContext, ThemeUsedContext } from '../lib/ThemeProvider'
+import { useLogout } from "../lib/auth";
+import { useRouter } from "next/router";
+import { useContext, useState } from "react";
+
+const AccountSettings = () => {
+  const logout = useLogout()
+
+  const theme = useContext(ThemeUsedContext)
+  const toggleTheme = useContext(ThemeUpdateContext)
+
+  const router = useRouter()
+  const editProfile = () => router.push(`/profile/edit`)
+
+  const [fill, setFill] = useState(0)
+
+  return (
+    <div className="center">
+      <div className="btn-column">
+        <Button color="grey" onClick={logout}>
+          <LogoutIcon />
+          Logout
+        </Button>
+        <Button color="grey" onClick={toggleTheme}>
+          {theme === 'light' ? <MoonIcon/> : <SunIcon />}
+          Switch Theme
+        </Button>
+        <Button color="green" onClick={editProfile}>
+          <ProfileIcon />
+          Edit Profile
+        </Button>
+        <HoldButton speed={50} setFill={setFill} onEnd={() => console.log('end')}>
+          <Button color="red">
+            <DeleteIcon />
+            Delete Account
+          </Button>
+        </HoldButton>
+      </div>
+
+      <DeleteBar progress={fill}/>
+    </div>
+  )
+}
+
+export default AccountSettings
--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -1,37 +1,17 @@
 import { NextPage } from "next";
 import { useContext, useState } from "react";
+import AccountSettings from "../../components/AccountSettings";
 import Avatar from "../../components/Avatar";
-import Button from "../../components/Button";
 import PostFeed from "../../components/PostFeed";
 import { Tab, TabGroup } from "../../components/Tabs";
-import { getUserPosts, getUserWithUsername, useLogout } from "../../lib/auth";
+import { getUserPosts, getUserWithUsername } from "../../lib/auth";
 import { AuthContext } from "../../lib/AuthProvider";
 import { postToJSON } from "../../lib/firebase";
-import { ThemeUpdateContext, ThemeUsedContext } from '../../lib/ThemeProvider'
-import MoonIcon from '../../public/icons/moon18.svg'
-import SunIcon from '../../public/icons/sun18.svg'
-import ProfileIcon from '../../public/icons/profile.svg'
-import LogoutIcon from '../../public/icons/logout18.svg'
-import DeleteIcon from '../../public/icons/delete18.svg'
-import { useRouter } from "next/router";
-import { deleteClick } from "../../lib/toast";
-import HoldButton from "../../components/HoldButton";
-import ProgressBar from "../../components/ProgressBar";
-import DeleteBar from "../../components/DeleteBar";
 
 
 const ProfilePage: NextPage = ({ author, posts }: any) => {
   const user = useContext(AuthContext)
   const isUserPage = author.username === user?.username || false
-  const logout = useLogout()
-
-  const theme = useContext(ThemeUsedContext)
-  const toggleTheme = useContext(ThemeUpdateContext)
-
-  const router = useRouter()
-  const editProfile = () => router.push(`/profile/edit`)
-
-  const [fill, setFill] = useState(0)
 
   return (
     <>
@@ -53,30 +33,7 @@ const ProfilePage: NextPage = ({ author, posts }: any) => {
               <PostFeed posts={posts.filter((p: any) => p.state === 'draft')}></PostFeed>
             </Tab>
             <Tab id="settings" label="Settings">
-              <div className="center">
-                <div className="btn-column">
-                  <Button color="grey" onClick={logout}>
-                    <LogoutIcon />
-                    Logout
-                  </Button>
-                  <Button color="grey" onClick={toggleTheme}>
-                    {theme === 'light' ? <MoonIcon/> : <SunIcon />}
-                    Switch Theme
-                  </Button>
-                  <Button color="green" onClick={editProfile}>
-                    <ProfileIcon />
-                    Edit Profile
-                  </Button>
-                  <HoldButton speed={35} setFill={setFill} onEnd={() => console.log('end')}>
-                    <Button color="red">
-                      <DeleteIcon />
-                      Delete Account
-                    </Button>
-                  </HoldButton>
-                </div>
-
-                <DeleteBar progress={fill}/>
-              </div>
+              <AccountSettings />
             </Tab>
           </TabGroup>
         )}
