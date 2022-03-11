5cc6f66e47fca62b957a92af94d1a8916cd71ddb
Olivier Bruch <olivier.bruch@gmail.com>
2022-03-11 15:11:35 +0000

    some small changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,8 @@
 import { NextPage } from "next";
-import { useState } from "react";
-import Card from "../components/Card";
-import CropImage from "../components/CropImage";
 
 const About: NextPage = () => {
-  const [image, setImage]: any = useState(null)
-
   return (
-    <Card>
-      <CropImage src="default profile pic.jpg" onEnd={setImage}></CropImage>
-      <img src={image} />
-    </Card>
+    <h2>About Page</h2>
   )
 }
 
