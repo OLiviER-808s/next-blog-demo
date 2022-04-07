a19fc4da538daf4f58babfa5178e4838267ac7ea
Olivier Bruch <olivier.bruch@gmail.com>
2022-04-07 13:54:28 +0100

    added aanimation to scroll up button

--- components/scrollUpButton.tsx
+++ components/scrollUpButton.tsx
@@ -1,6 +1,28 @@
 import styles from '../styles/ScrollUpButton.module.css'
 import UpIcon from '../public/icons/up.svg'
 import { useEffect, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+
+const slideUp = {
+  hidden: {
+    y: '100vh',
+    opacity: 0
+  },
+  visible: {
+    y: '0',
+    opacity: 1,
+    transition: {
+      duration: 0.25
+    }
+  },
+  exit: {
+    y: '100vh',
+    opacity: 0,
+    transition: {
+      duration: 0.5
+    }
+  }
+}
 
 const ScrollUpButton = () => {
   const [position, setPostition] = useState(0)
@@ -13,11 +35,14 @@ const ScrollUpButton = () => {
 
   const scrollUp = () => window.scrollTo(0, 0)
 
-  return position > 0 ? (
-    <button onClick={scrollUp} className={styles.btn}>
-      <UpIcon></UpIcon>
-    </button>
-  ) : null
+  return <AnimatePresence initial={false} exitBeforeEnter={true} onExitComplete={() => null}>
+    {position > 0 ? (
+      <motion.button onClick={scrollUp} className={styles.btn}
+      variants={slideUp} initial="hidden" animate="visible" exit="exit">
+        <UpIcon></UpIcon>
+      </motion.button>
+    ) : null}
+  </AnimatePresence>
 }
 
 export default ScrollUpButton
