071da2e63d59a406e586b6fb5767ffc017c00f48
Olivier Bruch <olivier.bruch@gmail.com>
2022-04-08 09:08:57 +0100

    can delete comments

--- components/CommentFeed.tsx
+++ components/CommentFeed.tsx
@@ -1,12 +1,24 @@
 import { Comment } from "../models/Comment.model"
 import Card from "./Card"
 import styles from '../styles/CommentFeed.module.css'
+import DeleteIcon from '../public/icons/delete24.svg'
 import Avatar from "./Avatar"
 import Link from "next/link"
 import { useRouter } from "next/router"
+import IconButton from "./IconButton"
+import { useContext } from "react"
+import { AuthContext } from "../lib/AuthProvider"
+import { deleteDoc, doc } from "firebase/firestore"
+import { db } from "../lib/firebase"
 
 const Comment = ({ comment }: any) => {
   const router = useRouter()
+  const user = useContext(AuthContext)
+
+  const deleteComment = async () => {
+    const ref = doc(db, `comments/${comment.id}`)
+    deleteDoc(ref)
+  }
 
   return (
     <div className={styles.comment}>
@@ -19,6 +31,14 @@ const Comment = ({ comment }: any) => {
             <h4><Link href={`/profile/${comment.authorname}`}>{ comment.authorname }</Link></h4>
             <p>{ comment.content }</p>
           </div>
+          <div className="spacer"></div>
+          {user.username === comment.authorname && (
+            <div className={styles.delete}>
+              <IconButton delete onClick={deleteComment}>
+                <DeleteIcon />
+              </IconButton>
+            </div>
+          )}
         </div>
       </Card>
     </div>
