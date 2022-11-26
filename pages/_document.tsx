f044a2fe5ab7fb82feee49b280be2327519f3246
OLiviER <olivier.bruch@gmail.com>
2022-11-26 16:51:26 +0000

    changed font import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/2.0.0-alpha.2/cropper.css" integrity="sha512-5ZQRy5L3cl4XTtZvjaJRucHRPKaKebtkvCWR/gbYdKH67km1e18C1huhdAc0wSnyMwZLiO7nEa534naJrH6R/Q==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
       <body>
